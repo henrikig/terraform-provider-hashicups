c2932c09da1e4b3f087c8d25e91d57660e601c85
Henrik Irgens Gravdal <47174810+henrikig@users.noreply.github.com>
2024-05-27 09:04:08 +0200

    chore: remove sample docs and add updated ones

--- internal/provider/coffees_data_source.go
+++ internal/provider/coffees_data_source.go
@@ -19,6 +19,7 @@ var (
 // coffeesDataSourceModel maps the data source schema data.
 type coffeesDataSourceModel struct {
 	Coffees []coffeesModel `tfsdk:"coffees"`
+	ID      types.String   `tfsdk:"id"`
 }
 
 // coffeesModel maps coffees schema data.
@@ -55,35 +56,49 @@ func (d *coffeesDataSource) Metadata(_ context.Context, req datasource.MetadataR
 // Schema defines the schema for the data source.
 func (d *coffeesDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
+		Description: "Fetches the list of coffees.",
 		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description: "Placeholder identifier attribute.",
+				Computed:    true,
+			},
 			"coffees": schema.ListNestedAttribute{
-				Computed: true,
+				Description: "List of coffees.",
+				Computed:    true,
 				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
 						"id": schema.Int64Attribute{
-							Computed: true,
+							Description: "Numeric identifier of the coffee.",
+							Computed:    true,
 						},
 						"name": schema.StringAttribute{
-							Computed: true,
+							Description: "Product name of the coffee.",
+							Computed:    true,
 						},
 						"teaser": schema.StringAttribute{
-							Computed: true,
+							Description: "Fun tagline for the coffee.",
+							Computed:    true,
 						},
 						"description": schema.StringAttribute{
-							Computed: true,
+							Description: "Product description of the coffee.",
+							Computed:    true,
 						},
 						"price": schema.Float64Attribute{
-							Computed: true,
+							Description: "Suggested cost of the coffee.",
+							Computed:    true,
 						},
 						"image": schema.StringAttribute{
-							Computed: true,
+							Description: "URI for an image of the coffee.",
+							Computed:    true,
 						},
 						"ingredients": schema.ListNestedAttribute{
-							Computed: true,
+							Description: "List of ingredients in the coffee.",
+							Computed:    true,
 							NestedObject: schema.NestedAttributeObject{
 								Attributes: map[string]schema.Attribute{
 									"id": schema.Int64Attribute{
-										Computed: true,
+										Description: "Numeric identifier of the coffee ingredient.",
+										Computed:    true,
 									},
 								},
 							},
@@ -97,6 +112,45 @@ func (d *coffeesDataSource) Schema(_ context.Context, _ datasource.SchemaRequest
 
 // Read refreshes the Terraform state with the latest data.
 func (d *coffeesDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state coffeesDataSourceModel
+
+	coffees, err := d.client.GetCoffees()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Read HashiCups Coffees",
+			err.Error(),
+		)
+		return
+	}
+
+	// Map response body to model
+	for _, coffee := range coffees {
+		coffeeState := coffeesModel{
+			ID:          types.Int64Value(int64(coffee.ID)),
+			Name:        types.StringValue(coffee.Name),
+			Teaser:      types.StringValue(coffee.Teaser),
+			Description: types.StringValue(coffee.Description),
+			Price:       types.Float64Value(coffee.Price),
+			Image:       types.StringValue(coffee.Image),
+		}
+
+		for _, ingredient := range coffee.Ingredient {
+			coffeeState.Ingredients = append(coffeeState.Ingredients, coffeesIngredientsModel{
+				ID: types.Int64Value(int64(ingredient.ID)),
+			})
+		}
+
+		state.Coffees = append(state.Coffees, coffeeState)
+	}
+
+	state.ID = types.StringValue("placeholder")
+
+	// Set state
+	diags := resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
 // Configure adds the provider configured client to the data source.
